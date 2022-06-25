5e15b1a4d2975c0985f7263da492ab7fa7055b52
Junmark Chi <chijunmark97@gmail.com>
2022-06-25 09:48:14 +0800

    Add additional components for dashboard

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "../../styles/dashboard/dashboard.css";
 import Sidebar from "./sidebar";
+import Overview from "./pages/overview";
+import Cards from "./pages/cards";
+import Deposit from "./pages/deposit";
+import Withdraw from "./pages/withdraw";
 
 export default function Dashboard(props) {
   return (
@@ -11,6 +16,12 @@ export default function Dashboard(props) {
         balance={props.balance}
         onLogOut={props.onLogOut}
       />
+      <Routes>
+        <Route path="/overview" element={<Overview />} />
+        <Route path="/cards" element={<Cards />} />
+        <Route path="/deposit" element={<Deposit />} />
+        <Route path="/withdraw" element={<Withdraw />} />
+      </Routes>
     </div>
   );
 }
